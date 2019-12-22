4e4557c22f39383688444280041dbc74f0b0fbc5
Le Van Chien <levanchien11a1@gmail.com>
2019-12-22 15:15:57 +0700

    Lesson18_Baitap_Chien cau 1,3

--- Lesson18_BaiTap_Nhom1/src/main/java/view/Ex01.java
+++ Lesson18_BaiTap_Nhom1/src/main/java/view/Ex01.java
@@ -1,11 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package view;
 
+import java.awt.Button;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.KeyAdapter;
@@ -23,16 +20,15 @@ import util.SizeUtils;
 
 /**
  *
- * @author Mr.Chien
+ * @author VO DINH DUNG
  */
-public class Ex01 extends JFrame {
+public class Ex011 extends JFrame {
 
-    private static final String Ex01_Truncate_ICON = "E:\\64px_flower.png";
-    private static final String Ex01_Truncate_TITLE = "Ex01_Truncate";
-    private static final int Ex01_Truncate_APP_WIDTH = 480;
-    private static final int Ex01_Truncate_APP_HEIGHT = 380;
-    private static final int INITIAL_PADDING_X = 140;
-    private static final int INITIAL_PADDING_Y = 50;
+    private static final String Ex01_ICON = "E:\\64px_flower.png";
+    private static final String Ex01_TITLE = "Ex01_Truncate";
+    private static final int Ex01_WIDTH = 540;
+    private static final int Ex01_HEIGHT = 480;
+
     private final Container conn = getContentPane();
     private JLabel labelHeader;
     private JLabel labelInput;
@@ -41,122 +37,144 @@ public class Ex01 extends JFrame {
     private JTextField tfOutput;
     private JButton btSubmit;
 
-    public Ex01() {
+    public Ex011() {
         initComponents();
         initEvents();
     }
 
     public static void main(String[] args) {
-        Ex01 ex01 = new Ex01();
+        Ex011 ex01 = new Ex011();
         ex01.setVisible(true);
     }
 
     private void initComponents() {
+        // DEFAULT
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setResizable(false);
-        setTitle(Ex01_Truncate_TITLE);
-        setIconImage(ImageUtils.load(Ex01_Truncate_ICON));
-        setSize(Ex01_Truncate_APP_WIDTH, Ex01_Truncate_APP_HEIGHT);
+        setTitle(Ex01_TITLE);
+        setIconImage(ImageUtils.load(Ex01_ICON));
+        setSize(Ex01_WIDTH, Ex01_HEIGHT);
         setLocationRelativeTo(null);
-        conn.setBackground(Color.GRAY);
+        setResizable(false);
+        conn.setBackground(Color.WHITE);
         conn.setLayout(null);
 
+        final int VERTICAL_PADDING = 30;
+        final int INITIAL_HEADER_X_PADDING = 100;
+        final int INITIAL_HEADER_Y_PADDING = 30;
+
         labelHeader = new JLabel();
-        labelHeader.setText("Ex01_Truncate");
-        labelHeader.setFont(new Font("Tahoma", Font.PLAIN, 25));
-        labelHeader.setBounds(INITIAL_PADDING_X, INITIAL_PADDING_Y, 
+        labelHeader.setText("TRUNCATE_NUMBER");
+        labelHeader.setFont(new Font("Tahoma", Font.BOLD, 24));
+        labelHeader.setBounds(INITIAL_HEADER_X_PADDING, INITIAL_HEADER_Y_PADDING,
                 SizeUtils.getPreWidth(labelHeader), SizeUtils.getPreHeight(labelHeader));
-        labelHeader.setForeground(Color.GREEN);
         conn.add(labelHeader);
 
+        final int INITIAL_X_PADDING = 20;
+        final int INITIAL_Y_PADDING = 100;
         labelInput = new JLabel();
-        labelInput.setText("Nhập dãy số:");
-        labelInput.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        labelInput.setBounds(INITIAL_PADDING_X, INITIAL_PADDING_Y, 
+        labelInput.setText("Nhập dãy số : ");
+        labelInput.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        labelInput.setBounds(INITIAL_X_PADDING, INITIAL_Y_PADDING,
                 SizeUtils.getPreWidth(labelInput), SizeUtils.getPreHeight(labelInput));
         conn.add(labelInput);
 
-        tfInput = new JTextField();
-        tfInput.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        tfInput.setColumns(1);
-        tfInput.setBounds(INITIAL_PADDING_X,
-                SizeUtils.getHeight(labelHeader) + 10,
-                SizeUtils.getWidth(labelHeader),
-                SizeUtils.getHeight(labelInput));
-        conn.add(tfInput);
-
         labelResult = new JLabel();
-        labelResult.setText("Kết quả:");
-        labelResult.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        labelResult.setBounds(INITIAL_PADDING_X, INITIAL_PADDING_Y, SizeUtils.getPreWidth(labelResult),
-                SizeUtils.getPreHeight(labelResult));
+        labelResult.setText("Kết quả : ");
+        labelResult.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        labelResult.setBounds(INITIAL_X_PADDING + 20, INITIAL_Y_PADDING + 100,
+                SizeUtils.getPreWidth(labelResult), SizeUtils.getPreHeight(labelResult));
         conn.add(labelResult);
 
-        tfOutput = new JTextField();
-        tfOutput.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        tfOutput.setColumns(1);
-        tfOutput.setEditable(false);
-        tfOutput.setBounds(INITIAL_PADDING_X,
-                SizeUtils.getHeight(btSubmit) + 10,
+        tfInput = new JTextField();
+        tfInput.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        tfInput.setBounds(INITIAL_HEADER_X_PADDING + 50,
+                INITIAL_HEADER_Y_PADDING + VERTICAL_PADDING +25,
                 SizeUtils.getWidth(labelHeader),
-                SizeUtils.getHeight(labelResult));
-        conn.add(tfOutput);
+                36);
+        conn.add(tfInput);
 
-        final int VERTICAL_PADDING = 80;
         btSubmit = new JButton();
-        btSubmit.setText("Thực hiện:");
+        btSubmit.setFocusPainted(false);
+        btSubmit.setText("Thực hiện");
         btSubmit.setFont(new Font("Tahoma", Font.PLAIN, 20));
-        btSubmit.setBounds(INITIAL_PADDING_X + 80,
-                INITIAL_PADDING_Y+ VERTICAL_PADDING * 6,
+        btSubmit.setBounds(INITIAL_HEADER_X_PADDING + 300,
+                INITIAL_HEADER_Y_PADDING-32 + VERTICAL_PADDING * 6,
                 SizeUtils.getPreWidth(labelHeader) / 2,
                 36);
-        btSubmit.setFocusPainted(false);
         conn.add(btSubmit);
+
+        tfOutput = new JTextField();
+        tfOutput.setEditable(false);
+        tfOutput.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        tfOutput.setBounds(INITIAL_X_PADDING + 60 + SizeUtils.getWidth(labelResult),
+                INITIAL_HEADER_Y_PADDING + VERTICAL_PADDING
+                + SizeUtils.getWidth(labelInput)+20,
+                SizeUtils.getWidth(labelHeader), 36);
+        conn.add(tfOutput);
 
     }
 
     private void initEvents() {
-        setBtExecuteEvents();
-        setTfInputEvents();
+        btSubmitEvents();
+        tfOutputEvents();
     }
 
-    private void setBtExecuteEvents() {
-        btSubmit.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                String input = tfInput.getText();
-                String output = distinctNumbers(input);
-                tfOutput.setText(output);
-            }
-        });
-    }
-
-    private String distinctNumbers(String input) {
-        StringBuilder output = new StringBuilder();
-        try {
-            Pattern.compile(",")
-                    .splitAsStream(input)
-                    .mapToInt(s -> Integer.parseInt(s.trim()))
-                    .distinct()
-                    .forEach(number -> output.append(number).append(","));
-        } catch (NumberFormatException e) {
-            return "Nhập lại";
-        }
-
-        String outputString = output.toString();
-        return (outputString.substring(0, outputString.length() - 1));
-    }
-
-    private void setTfInputEvents() {
+    private void tfOutputEvents() {
         tfInput.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    String input = tfInput.getText();
-                    String output = distinctNumbers(input);
-                    tfOutput.setText(output);
+                    tfInput.setText(convertextToNumber(tfInput.getText()));
                 }
             }
         });
     }
+
+    private void btSubmitEvents() {
+        Font btSubmitFont = btSubmit.getFont();
+        btSubmit.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                tfOutput.setText(convertextToNumber(tfInput.getText()));
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                Font hightlightFont = new Font(btSubmitFont.getName(), Font.BOLD, btSubmitFont.getSize());
+                btSubmit.setFont(hightlightFont);
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                btSubmit.setFont(btSubmitFont);
+            }
+        });
+    }
+
+    private String convertextToNumber(String input) {
+        String[] arr = input.split("\\,");
+        String result = "";
+        for (int i = 0; i < arr.length; i++) {
+            if (isUnique(arr, arr[i])) {
+                result += arr[i] + " ";
+            }
+        }
+        return result;
+    }
+
+    private static boolean isUnique(String[] arr, String s) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (s.equals(arr[i])) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
