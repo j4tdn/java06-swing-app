f57620ab6795d5cb289efc3996bdf977e234177c
Viet Anh <darksider2699@gmail.com>
2019-12-22 22:14:40 +0700

    Viet Anh - Bai tap 2,4

---
+++ Lesson18_BaiTap_Nhom3/src/main/java/utils/ImageUtils.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author MyPC
+ */
+public class ImageUtils {
+
+    private static final String TITLE_DEFAULT_IMAGE="F:\\NetbeanProject\\Lesson18\\src\\main\\java\\image\\64px_flower.png";
+    private ImageUtils() {
+    }
+
+    public static Image load(String path) {
+        if(path==null||path.isEmpty()){
+            path = TITLE_DEFAULT_IMAGE;
+        }
+        ImageIcon icon = new ImageIcon(path);
+        return icon.getImage();
+    }
+}
---
+++ Lesson18_BaiTap_Nhom3/src/main/java/utils/SizeUtils.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.awt.Component;
+
+/**
+ *
+ * @author MyPC
+ */
+public class SizeUtils {
+
+    private SizeUtils() {
+
+    }
+
+    public static int getPreWidth(Component comp) {
+        return (int) comp.getPreferredSize().getWidth();
+    }
+
+    public static int getPreHeight(Component comp) {
+        return (int) comp.getPreferredSize().getHeight();
+    }
+
+    public static int getWidth(Component comp) {
+        return (int) comp.getSize().getWidth();
+    }
+
+}
